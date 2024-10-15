556fad263caa6e69b0c578815d59833ef7f0ebbd
Vinicius Jose De Biasi <debiasivj@gmail.com>
2024-10-15 00:26:19 -0300

    added build for production

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,26 @@ import {
   ListBulletIcon,
   TrashIcon,
   PlusCircledIcon,
-  ArrowUpIcon,
+  ArrowUpIcon
 } from "@radix-ui/react-icons";
 
 import "./App.css";
 
 function App() {
-  const [task, setTask] = useState("");
-  const [savedTasks, setSavedTasks] = useState([]);
+  const [task, setTask] = useState(""); // Armazena o valor da nova tarefa
+  const [savedTasks, setSavedTasks] = useState([]); // Armazena todas as tarefas
   const [showFavorites, setShowFavorites] = useState(false);
   const [showCompleteds, setShowCompleteds] = useState(false);
   const [message, setMessage] = useState("");
 
+  // Função para incluir uma nova tarefa
   const IncludeTask = (event) => {
     event.preventDefault();
 
     if (!task) {
       setMessage(
         <>
-          <h3 className="message">
-            {" "}
-            Please enter a task <ArrowUpIcon className="icons" />
-          </h3>
+          <h3 className="message"> Please enter a task <ArrowUpIcon className="icons" /></h3>
         </>
       );
     } else {
@@ -39,85 +37,83 @@ function App() {
       const NewTask = {
         id: Date.now(),
         name: task,
-        favorite: false,
-        completed: false,
+        favorite: false, // Adiciona um campo para "favoritar"
+        completed: false, // Adiciona um campo para "concluída"
       };
 
       const UpdatedTasks = [...ExistingTasks, NewTask];
-      localStorage.setItem("tasks", JSON.stringify(UpdatedTasks));
+      localStorage.setItem("tasks", JSON.stringify(UpdatedTasks)); // Atualiza o LocalStorage
 
-      setTask("");
-      setSavedTasks(UpdatedTasks);
+      setTask(""); // Limpa o input
+      setSavedTasks(UpdatedTasks); // Atualiza o estado com a nova lista de tarefas
     }
   };
 
+  // Função para exibir as tarefas ao carregar a página
   const ShowTasks = () => {
-    setShowFavorites(false);
-    setShowCompleteds(false);
+    setShowFavorites(false)
+    setShowCompleteds(false)
     const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    setSavedTasks(savedTasks);
+    setSavedTasks(savedTasks); // Atualiza o estado com as tarefas salvas
   };
 
+  // Função para mostrar tarefas completas
   const ShowCompletedTasks = () => {
-    setShowCompleteds(true);
-    setShowFavorites(false);
+    setShowCompleteds(true); // Define para mostrar apenas as tarefas favoritas
+    setShowFavorites(false)
     const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    setSavedTasks(savedTasks);
+    setSavedTasks(savedTasks); // Atualiza o estado com as tarefas salvas
   };
 
+  // Função para mostrar tarefas favoritas
   const ShowFavoriteTasks = () => {
-    setShowFavorites(true);
-    setShowCompleteds(false);
+    setShowFavorites(true); // Define para mostrar apenas as tarefas favoritas
+    setShowCompleteds(false)
     const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    setSavedTasks(savedTasks);
+    setSavedTasks(savedTasks); // Atualiza o estado com as tarefas salvas
   };
 
+  // Função para remover uma tarefa
   const RemoveTask = (taskId) => {
     const RemainingTasks = savedTasks.filter((task) => task.id !== taskId);
     localStorage.setItem("tasks", JSON.stringify(RemainingTasks));
-    setSavedTasks(RemainingTasks);
+    setSavedTasks(RemainingTasks); // Atualiza o estado
   };
 
+  // Função para marcar/desmarcar uma tarefa como favorita
   const ToggleFavorite = (taskId) => {
     const UpdatedTasks = savedTasks.map((task) => {
       if (task.id === taskId) {
-        return { ...task, favorite: !task.favorite };
+        return { ...task, favorite: !task.favorite }; // Alterna o status de favorito
       }
       return task;
     });
     localStorage.setItem("tasks", JSON.stringify(UpdatedTasks));
-    setSavedTasks(UpdatedTasks);
+    setSavedTasks(UpdatedTasks); // Atualiza o estado
   };
 
+  // Função para marcar/desmarcar uma tarefa como completa
   const ToggleCompleted = (taskId) => {
     const UpdatedTasks = savedTasks.map((task) => {
       if (task.id === taskId) {
-        return { ...task, completed: !task.completed };
+        return { ...task, completed: !task.completed }; // Alterna o status de favorito
       }
       return task;
     });
     localStorage.setItem("tasks", JSON.stringify(UpdatedTasks));
-    setSavedTasks(UpdatedTasks);
+    setSavedTasks(UpdatedTasks); // Atualiza o estado
   };
 
   return (
     <div className="main">
       <div className="container-choose">
-        <button
-          className="button-choose"
-          style={{ fontWeight: "bold" }}
-          onClick={ShowFavoriteTasks}
-        >
+        <button className="button-choose" style={{ fontWeight: "bold" }} onClick={ShowFavoriteTasks}>
           {" "}
           <HeartFilledIcon className="icons" />
           &nbsp;&nbsp;&nbsp;<p>Favorites</p>{" "}
         </button>
 
-        <button
-          className="button-choose"
-          style={{ fontWeight: "bold" }}
-          onClick={ShowCompletedTasks}
-        >
+        <button className="button-choose" style={{ fontWeight: "bold" }} onClick={ShowCompletedTasks}>
           {" "}
           <CheckCircledIcon className="icons" />
           &nbsp;&nbsp;&nbsp;<p>Completed</p>{" "}
@@ -161,40 +157,33 @@ function App() {
 
         <div className="tasks">
           {savedTasks
-            .filter(
-              (task) =>
-                (!showFavorites && !showCompleteds) ||
-                (showFavorites && task.favorite) ||
-                (showCompleteds && task.completed)
-            )
-            .map((task) => (
-              <p className="task" key={task.id}>
-                <input
-                  type="checkbox"
-                  checked={task.completed}
-                  className="input-checkbox"
-                  onChange={() => ToggleCompleted(task.id)}
-                />
+          .filter((task) => (!showFavorites && !showCompleteds) || // Exibe todas as tarefas
+          (showFavorites && task.favorite) || // Exibe apenas favoritas
+          (showCompleteds && task.completed))
+          .map((task) => (
+            <p className="task" key={task.id}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+                className="input-checkbox"
+                onChange={() => ToggleCompleted(task.id)}
+              />
 
-                {task.name}
-                <button
-                  className="button-task"
-                  onClick={() => ToggleFavorite(task.id)}
-                >
-                  {task.favorite ? (
-                    <HeartFilledIcon className="icons" />
-                  ) : (
-                    <HeartIcon className="icons" />
-                  )}
-                </button>
-                <button
-                  className="button-task"
-                  onClick={() => RemoveTask(task.id)}
-                >
-                  <TrashIcon className="icons" />
-                </button>
-              </p>
-            ))}
+              {task.name}
+              <button
+                className="button-task"
+                onClick={() => ToggleFavorite(task.id)}
+              >
+                {task.favorite ? <HeartFilledIcon className="icons" /> : <HeartIcon className="icons" /> }
+              </button>
+              <button
+                className="button-task"
+                onClick={() => RemoveTask(task.id)}
+              >
+                <TrashIcon className="icons" />
+              </button>
+            </p>
+          ))}
         </div>
       </div>
     </div>
