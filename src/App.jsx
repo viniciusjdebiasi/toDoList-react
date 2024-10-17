f837e4435a1b33992d56c821e492d7a777aa8535
Vinicius Jose De Biasi <debiasivj@gmail.com>
2024-10-17 18:56:28 -0300

    added aos library to project

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,21 @@ import {
   ListBulletIcon,
   TrashIcon,
   PlusCircledIcon,
-  ArrowUpIcon
+  ArrowUpIcon,
 } from "@radix-ui/react-icons";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import "./App.css";
 
 function App() {
-  const [task, setTask] = useState(""); 
-  const [savedTasks, setSavedTasks] = useState([]); 
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
+  const [task, setTask] = useState("");
+  const [savedTasks, setSavedTasks] = useState([]);
   const [showFavorites, setShowFavorites] = useState(false);
   const [showCompleteds, setShowCompleteds] = useState(false);
   const [message, setMessage] = useState("");
@@ -25,7 +32,10 @@ function App() {
     if (!task) {
       setMessage(
         <>
-          <h3 className="message"> Please enter a task <ArrowUpIcon className="icons" /></h3>
+          <h3 className="message">
+            {" "}
+            Please enter a task <ArrowUpIcon className="icons" />
+          </h3>
         </>
       );
     } else {
@@ -36,37 +46,37 @@ function App() {
       const NewTask = {
         id: Date.now(),
         name: task,
-        favorite: false, 
-        completed: false, 
+        favorite: false,
+        completed: false,
       };
 
       const UpdatedTasks = [...ExistingTasks, NewTask];
-      localStorage.setItem("tasks", JSON.stringify(UpdatedTasks)); 
+      localStorage.setItem("tasks", JSON.stringify(UpdatedTasks));
 
-      setTask(""); 
-      setSavedTasks(UpdatedTasks); 
+      setTask("");
+      setSavedTasks(UpdatedTasks);
     }
   };
 
   const ShowTasks = () => {
-    setShowFavorites(false)
-    setShowCompleteds(false)
+    setShowFavorites(false);
+    setShowCompleteds(false);
     const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    setSavedTasks(savedTasks); 
+    setSavedTasks(savedTasks);
   };
 
   const ShowCompletedTasks = () => {
-    setShowCompleteds(true); 
-    setShowFavorites(false)
+    setShowCompleteds(true);
+    setShowFavorites(false);
     const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    setSavedTasks(savedTasks); 
+    setSavedTasks(savedTasks);
   };
 
   const ShowFavoriteTasks = () => {
-    setShowFavorites(true); 
-    setShowCompleteds(false)
+    setShowFavorites(true);
+    setShowCompleteds(false);
     const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    setSavedTasks(savedTasks); 
+    setSavedTasks(savedTasks);
   };
 
   const RemoveTask = (taskId) => {
@@ -78,35 +88,43 @@ function App() {
   const ToggleFavorite = (taskId) => {
     const UpdatedTasks = savedTasks.map((task) => {
       if (task.id === taskId) {
-        return { ...task, favorite: !task.favorite }; 
+        return { ...task, favorite: !task.favorite };
       }
       return task;
     });
     localStorage.setItem("tasks", JSON.stringify(UpdatedTasks));
-    setSavedTasks(UpdatedTasks); 
+    setSavedTasks(UpdatedTasks);
   };
 
   const ToggleCompleted = (taskId) => {
     const UpdatedTasks = savedTasks.map((task) => {
       if (task.id === taskId) {
-        return { ...task, completed: !task.completed }; 
+        return { ...task, completed: !task.completed };
       }
       return task;
     });
     localStorage.setItem("tasks", JSON.stringify(UpdatedTasks));
-    setSavedTasks(UpdatedTasks); 
+    setSavedTasks(UpdatedTasks);
   };
 
   return (
-    <div className="main">
+    <div className="main" data-aos="fade-down">
       <div className="container-choose">
-        <button className="button-choose" style={{ fontWeight: "bold" }} onClick={ShowFavoriteTasks}>
+        <button
+          className="button-choose"
+          style={{ fontWeight: "bold" }}
+          onClick={ShowFavoriteTasks}
+        >
           {" "}
           <HeartFilledIcon className="icons" />
           &nbsp;&nbsp;&nbsp;<p>Favorites</p>{" "}
         </button>
 
-        <button className="button-choose" style={{ fontWeight: "bold" }} onClick={ShowCompletedTasks}>
+        <button
+          className="button-choose"
+          style={{ fontWeight: "bold" }}
+          onClick={ShowCompletedTasks}
+        >
           {" "}
           <CheckCircledIcon className="icons" />
           &nbsp;&nbsp;&nbsp;<p>Completed</p>{" "}
@@ -150,35 +168,42 @@ function App() {
 
         {message}
 
-        <div className="tasks">
+        <div className="tasks" >
           {savedTasks
-          .filter((task) => (!showFavorites && !showCompleteds) || 
-          (showFavorites && task.favorite) || 
-          (showCompleteds && task.completed))
-          .map((task) => (
-            <p className="task" key={task.id}>
-              <input
-                type="checkbox"
-                checked={task.completed}
-                className="input-checkbox"
-                onChange={() => ToggleCompleted(task.id)}
-              />
+            .filter(
+              (task) =>
+                (!showFavorites && !showCompleteds) ||
+                (showFavorites && task.favorite) ||
+                (showCompleteds && task.completed)
+            )
+            .map((task) => (
+              <p className="task" key={task.id}>
+                <input
+                  type="checkbox"
+                  checked={task.completed}
+                  className="input-checkbox"
+                  onChange={() => ToggleCompleted(task.id)}
+                />
 
-              {task.name}
-              <button
-                className="button-task"
-                onClick={() => ToggleFavorite(task.id)}
-              >
-                {task.favorite ? <HeartFilledIcon className="icons" /> : <HeartIcon className="icons" /> }
-              </button>
-              <button
-                className="button-task"
-                onClick={() => RemoveTask(task.id)}
-              >
-                <TrashIcon className="icons" />
-              </button>
-            </p>
-          ))}
+                {task.name}
+                <button
+                  className="button-task"
+                  onClick={() => ToggleFavorite(task.id)}
+                >
+                  {task.favorite ? (
+                    <HeartFilledIcon className="icons" />
+                  ) : (
+                    <HeartIcon className="icons" />
+                  )}
+                </button>
+                <button
+                  className="button-task"
+                  onClick={() => RemoveTask(task.id)}
+                >
+                  <TrashIcon className="icons" />
+                </button>
+              </p>
+            ))}
         </div>
       </div>
     </div>
